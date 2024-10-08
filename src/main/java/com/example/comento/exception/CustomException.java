00cf92de7e60cfb0d33620e9009650dadaa7b535
jher235 <tim668666@gmail.com>
2024-10-08 12:42:23 +0900

    refactor: CustomException을 바로 바로 사용할 수 없도록 추상 클래스로 분리, 생성자도 자식 클래스만 작성할 수 있도록 protected로 설정.

--- src/main/java/com/example/comento/exception/CustomException.java
+++ src/main/java/com/example/comento/exception/CustomException.java
@@ -4,16 +4,16 @@ import com.example.comento.exception.errorcode.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends RuntimeException{
+public abstract class CustomException extends RuntimeException{
     private final ErrorCode errorCode;
     private final String detail;
 
-    public CustomException(ErrorCode errorCode){
+    protected CustomException(ErrorCode errorCode){
       this.errorCode = errorCode;
       this.detail = null;
     }
 
-    public CustomException(ErrorCode errorCode, String detail){
+    protected CustomException(ErrorCode errorCode, String detail){
         this.errorCode = errorCode;
         this.detail = detail;
     }
