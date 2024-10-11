68f48514dc5c3664d01ea6a0e603310a02e545c0
jher235 <tim668666@gmail.com>
2024-10-11 17:50:48 +0900

    feat: 쿠키 관련 유틸 클래스 구현 - #13

---
+++ src/main/java/com/example/comento/global/util/CookieUtil.java
@@ -0,0 +1,36 @@
+package com.example.comento.global.util;
+
+import com.example.comento.global.exception.UnauthorizedException;
+import com.example.comento.global.exception.errorcode.ErrorCode;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseCookie;
+
+import java.time.Duration;
+
+public class CookieUtil {
+    private CookieUtil(){}
+    public static ResponseCookie createTokenCookie(
+            String tokenName, String tokenValue, Duration maxAge, String path){
+        return ResponseCookie.from(tokenName, tokenValue)
+                .maxAge(maxAge)
+                .path(path)
+                .httpOnly(true)
+                .sameSite("None")
+                .secure(true)
+                .build();
+    }
+
+    public static Cookie getCookie(final HttpServletRequest request, final String cookieName) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(cookieName)) {
+                    return cookie;
+                }
+            }
+        }
+        throw new UnauthorizedException(ErrorCode.TOKEN_NOT_FOUND);
+    }
+}
