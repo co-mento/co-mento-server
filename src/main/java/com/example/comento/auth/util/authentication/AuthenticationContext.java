22b041a9c027a58500ae8ffcd2237b6942ab5a3c
jher235 <tim668666@gmail.com>
2024-10-11 22:31:15 +0900

    feat: 인증된 유저 정보를 받아오고 저장하기 - #13

---
+++ src/main/java/com/example/comento/auth/util/authentication/AuthenticationContext.java
@@ -0,0 +1,16 @@
+package com.example.comento.auth.util.authentication;
+
+import com.example.comento.user.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Setter
+@Getter
+@Component
+@RequestScope
+public class AuthenticationContext {
+    private User principal;
+}
