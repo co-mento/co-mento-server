2cccb95f12033e379cc51b9150fae72b1acd0ea1
jher235 <tim668666@gmail.com>
2024-10-11 22:37:06 +0900

    feat: 유저 생성 시 유저 프로필 함께 생성되도록 작성 - #13

--- src/main/java/com/example/comento/user/domain/User.java
+++ src/main/java/com/example/comento/user/domain/User.java
@@ -11,14 +11,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
-    @Column(unique = true)
-    private String email;
+    @Column(nullable = false, unique = true)
+    private String userId;
+
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
     @Column(nullable = false, updatable = false)
     private String salt;
 
+    @PrimaryKeyJoinColumn
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private UserProfile userProfile;
+
+    public User (String userId, String password, String salt, String email, String name){
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.salt = salt;
+        this.userProfile = new UserProfile(this, name);
+    }
 
 }
