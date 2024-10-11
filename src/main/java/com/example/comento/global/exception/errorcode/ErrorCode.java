5b050d5acbe651552e8a7e1304e34860739d7a21
jher235 <tim668666@gmail.com>
2024-10-11 17:51:43 +0900

    feat: 예외 코드 추가 -#13 INVALID_TOKEN, TOKEN_NOT_FOUND, USER_NOT_FOUND

--- src/main/java/com/example/comento/global/exception/errorcode/ErrorCode.java
+++ src/main/java/com/example/comento/global/exception/errorcode/ErrorCode.java
@@ -15,6 +15,15 @@ public enum ErrorCode {
     LENGTH("4003", "길이가 유효하지 않습니다."),
     EMAIL("4004", "이메일 형식이 유효하지 않습니다."),
     NOT_NULL("4005", "필수값이 공백입니다."),
+
+    //Unauthorized
+    INVALID_TOKEN("4011", "유효하지 않은 토큰입니다."),
+    TOKEN_NOT_FOUND("4012", "토큰이 존재하지 않습니다."),
+
+    //Not_Found
+    USER_NOT_FOUND("4040", "존재하지 않는 유저입니다"),
+
+
     ;
 
 
