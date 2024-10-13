4818397093f68f15871dddbf49cf21473b876058
jher235 <tim668666@gmail.com>
2024-10-13 13:52:43 +0900

    feat: 로그인 기능 구현 - #15

--- src/main/java/com/example/comento/auth/controller/AuthController.java
+++ src/main/java/com/example/comento/auth/controller/AuthController.java
@@ -1,8 +1,14 @@
 package com.example.comento.auth.controller;
 
+import com.example.comento.auth.dto.request.LoginRequest;
 import com.example.comento.auth.dto.request.SignUpRequest;
+import com.example.comento.auth.dto.response.TokenResponseCookies;
 import com.example.comento.auth.service.AuthService;
+import com.example.comento.auth.service.TokenService;
 import com.example.comento.global.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,16 +18,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+    private final TokenService tokenService;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입 api")
     public ResponseEntity<ResponseDto<Void>> signUp(@Valid @RequestBody SignUpRequest request){
         authService.signUp(request);
-        return new ResponseEntity<>(ResponseDto.res(true, "회원가입 성공 "), HttpStatus.CREATED);
+        return new ResponseEntity<>(ResponseDto.res(true, "회원가입 성공"), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인 api")
+    public ResponseEntity<ResponseDto<Void>> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response){
+        UUID id = authService.login(request);
+        TokenResponseCookies cookies = tokenService.issueToken(id.toString());
+        response.addHeader("set-cookie", cookies.getAccessToken().toString());
+        return new ResponseEntity<>(ResponseDto.res(true, "로그인 성공"), HttpStatus.OK);
     }
 
 
