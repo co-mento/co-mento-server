5c12564e0677bb3fb485da7512a738696b1e8795
jher235 <tim668666@gmail.com>
2024-10-14 00:44:15 +0900

    feat: 유저 랭킹 가져오는 메서드 작성 -#21 Jpa 자동 메서드로도 생성 가능했으나 정렬 기준을 하나 더 주기 위해서 쿼리를 사용함. ++ 내 공부용 ㅎㅎ

--- src/main/java/com/example/comento/user/repository/UserProfileJpaRepository.java
+++ src/main/java/com/example/comento/user/repository/UserProfileJpaRepository.java
@@ -1,11 +1,19 @@
 package com.example.comento.user.repository;
 
 import com.example.comento.user.domain.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserProfileJpaRepository extends JpaRepository<UserProfile, UUID> {
+
+    @Query("select up " +
+            "from user_profile as up " +
+            "order by up.experience DESC, up.name ")
+    public Page<UserProfile> getUserRanking(Pageable pageable);
 }
