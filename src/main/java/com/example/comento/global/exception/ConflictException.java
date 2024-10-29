1bd9d16dacd7fd93575aee1299a9fc2d8438b16e
jher235 <tim668666@gmail.com>
2024-10-29 15:26:54 +0900

    feat: 이미 좋아요를 누른 문제에 대한 예외처리 - #29

--- src/main/java/com/example/comento/global/exception/ConflictException.java
+++ src/main/java/com/example/comento/global/exception/ConflictException.java
@@ -6,4 +6,8 @@ public class ConflictException extends CustomException{
     public ConflictException(ErrorCode errorCode) {
         super(errorCode);
     }
+
+    public ConflictException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
 }
