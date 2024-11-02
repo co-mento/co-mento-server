16c6829cf76b91b5759c94b8408cc1656d961bdd
jher235 <tim668666@gmail.com>
2024-11-02 23:19:50 +0900

    feat: 솔루션이 존재하지 않는 경우 예외 추가 - #33

--- src/main/java/com/example/comento/global/exception/errorcode/ErrorCode.java
+++ src/main/java/com/example/comento/global/exception/errorcode/ErrorCode.java
@@ -25,7 +25,8 @@ public enum ErrorCode {
     //Not_Found
     USER_NOT_FOUND("4040", "존재하지 않는 유저입니다."),
     PAGE_NOT_FOUND("4041", "존재하지 않는 페이지입니다."),
-    PROBLEM_NOT_FOUNT("4042", "존재하지 않는 문제입니다."),
+    PROBLEM_NOT_FOUND("4042", "존재하지 않는 문제입니다."),
+    SOLUTION_NOT_FOUND("4043", "존재하지 않는 솔루션입니다"),
 
     //Conflict
     LIKE_CONFLICT("4090", "불가한 좋아요 요청입니다.")
