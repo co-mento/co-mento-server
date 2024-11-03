8683f6534cba6b59ea761504cf152163b4a31f98
vr121170 <82792543+vr121170@users.noreply.github.com>
2024-11-03 09:32:02 +0900

    feat: 문제 작성, 수정 삭제, 전체조회, 단일조회 기능

--- src/main/java/com/example/comento/problem/controller/ProblemController.java
+++ src/main/java/com/example/comento/problem/controller/ProblemController.java
@@ -16,6 +16,43 @@ import org.springframework.web.bind.annotation.*;
 public class ProblemController {
 
     private final ProblemLikeService problemLikeService;
+    private final ProblemService problemService;
+
+    @PostMapping
+    @Operation(summary = "문제 작성 api")
+    public ResponseEntity<ResponseDto<Problem>> createProblem(@RequestBody Problem problem) {
+        Problem createdProblem = problemService.createProblem(problem);
+        return new ResponseEntity<>(ResponseDto.res(true, "문제 작성 성공", createdProblem), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{problem-id}")
+    @Operation(summary = "문제 수정 api")
+    public ResponseEntity<ResponseDto<Problem>> updateProblem(@PathVariable("problem-id") Long problemId,
+                                                              @RequestBody Problem updatedProblem) {
+        Problem updated = problemService.updateProblem(problemId, updatedProblem);
+        return new ResponseEntity<>(ResponseDto.res(true, "문제 수정 성공", updated), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{problem-id}")
+    @Operation(summary = "문제 삭제 api")
+    public ResponseEntity<ResponseDto<Void>> deleteProblem(@PathVariable("problem-id") Long problemId) {
+        problemService.deleteProblem(problemId);
+        return new ResponseEntity<>(ResponseDto.res(true, "문제 삭제 성공"), HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    @Operation(summary = "모든 문제 조회 api")
+    public ResponseEntity<ResponseDto<List<Problem>>> getAllProblems() {
+        List<Problem> problems = problemService.getAllProblems();
+        return new ResponseEntity<>(ResponseDto.res(true, "모든 문제 조회 성공", problems), HttpStatus.OK);
+    }
+
+    @GetMapping("/{problem-id}")
+    @Operation(summary = "단일 문제 조회 api")
+    public ResponseEntity<ResponseDto<Problem>> getProblem(@PathVariable("problem-id") Long problemId) {
+        Problem problem = problemService.getProblem(problemId);
+        return new ResponseEntity<>(ResponseDto.res(true, "문제 조회 성공", problem), HttpStatus.OK);
+    }
 
     @PostMapping("/{problem-id}/like")
     @Operation(summary = "좋아요 추가 api")
@@ -28,7 +65,7 @@ public class ProblemController {
     @DeleteMapping("/{problem-id}/like")
     @Operation(summary = "좋아요 삭제 api")
     public ResponseEntity<ResponseDto<Void>> unLikeProblem(@AuthenticationPrincipal Principal principal,
-                                                         @PathVariable("problem-id") Long problemId){
+                                                           @PathVariable("problem-id") Long problemId){
         problemLikeService.unLikeProblem(principal.getProfile(), problemId);
         return new ResponseEntity<>(ResponseDto.res(true, "좋아요 삭제 성공"), HttpStatus.OK);
     }
