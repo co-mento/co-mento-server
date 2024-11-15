4712f70e33952e24fe25871fc3fcb05e4b88dea5
vr121170 <82792543+vr121170@users.noreply.github.com>
2024-11-15 10:07:13 +0900

    feat: 카테고리 작성, 수정 삭제, 전체조회, 단일조회 기능

---
+++ src/main/java/com/example/comento/category/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.example.comento.category.controller;
+
+import com.example.comento.category.domain.Category;
+import com.example.comento.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/problems/algorithms")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody String name) {
+        Category category = categoryService.createCategory(name);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable UUID id) {
+        Category category = categoryService.getCategory(id);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable UUID id, @RequestBody String newName) {
+        Category updatedCategory = categoryService.updateCategory(id, newName);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable UUID id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
