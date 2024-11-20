c8d6451b06f36f62f7260b5adacb13bb18d1deba
jher235 <tim668666@gmail.com>
2024-11-21 01:00:22 +0900

    feat: SolvedStatus를 문제와 프로필로 찾는 메서드 작성 - #57

--- src/main/java/com/example/comento/solvedstatus/repository/SolvedStatusRepository.java
+++ src/main/java/com/example/comento/solvedstatus/repository/SolvedStatusRepository.java
@@ -1,11 +1,16 @@
 package com.example.comento.solvedstatus.repository;
 
+import com.example.comento.problem.damain.Problem;
 import com.example.comento.solvedstatus.domain.SolvedStatus;
+import com.example.comento.user.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SolvedStatusRepository extends JpaRepository<SolvedStatus, UUID> {
+
+    public Optional<SolvedStatus> findSolvedStatusByProblemAndUserProfile(Problem problem, UserProfile profile);
 }
